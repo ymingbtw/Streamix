e368d7c82ccae4cbbed63257d0eb6e577b8c5d8d
Yming <ymingbytheway@gmail.com>
2025-06-19 15:19:50 +0700

    'hi'

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
-        $middleware->append([HandleCors::class]);
+        $middleware->append([]);
         $middleware->group('web', []);
         $middleware->group('api', []);
     })
