62467f4cc07c0b147090622386399e882cc029ed
Yming <ymingbytheway@gmail.com>
2025-06-19 20:50:14 +0700

    'd'

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response('not found');
 });
-Route::get('/movies/{movie}', function ($movie) {
-    return response()->file(base_path() . '/movies/' . $movie . '/index.m3u8');
-});
-Route::get('/movies/{movie}/{res}/{file}', function ($movie, $res, $file) {
-    return response()->file(
-        base_path() . '/movies/' . $movie . '/' . $res . '/' . $file
-    );
-});
+// Route::get('/movies/{movie}', function ($movie) {
+//     return response()->file(base_path() . '/movies/' . $movie . '/index.m3u8');
+// });
+// Route::get('/movies/{movie}/{res}/{file}', function ($movie, $res, $file) {
+//     return response()->file(
+//         base_path() . '/movies/' . $movie . '/' . $res . '/' . $file
+//     );
+// });
 
 Route::fallback(function () {
     return redirect('/'); // or a 404 view or custom page
